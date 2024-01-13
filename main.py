c39041d66f47473bd3ff5395c6cd7ad2d7031159
iamkishorekumar-git <iamkishorekumar1@gmail.com>
2024-01-13 12:08:15 +0530

    Updated main code

--- main.py
+++ main.py
@@ -14,17 +14,18 @@ res = workflow.getWorkflowJobs(org,repo,github_token,7501651743)
 
 target_jobs = ["unit-test-job", "cypress-run-job"]
 
-output_lines = []
 
-output_lines = []
+output_jobs = []
 
 for x in res["jobs"]:
     if x["name"] in target_jobs:
-        output_lines.append(f"Job Name: {x['name']}")
-        output_lines.append(f"HTML URL: {x['html_url']}")
-        output_lines.append(f"Status: {x['status']}")
-        output_lines.append("---")  # Separator between job outputs
+        job_info = {
+            "Job Name": x["name"],
+            "HTML URL": x["html_url"],
+            "Status": x["status"]
+        }
+        output_jobs.append(job_info)
 
-output = "\n".join(output_lines)  # Combine lines with newlines
-print(f"::set-output name=my_output::{output}")  # Set output in single line
+
+print(f"my_output={output_jobs} >> $GITHUB_OUTPUT")
 

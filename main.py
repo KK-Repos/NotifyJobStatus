1f36ebdca1040d690327b748c37c12040c5075d6
iamkishorekumar-git <iamkishorekumar1@gmail.com>
2024-01-13 11:25:31 +0530

    Updated main code

--- main.py
+++ main.py
@@ -11,12 +11,17 @@ branch_name = os.environ.get("BRANCH_NAME")
 # workflow.trigger_workflow(org, repo, github_token, branch_name)
 
 res = workflow.getWorkflowJobs(org,repo,github_token,7501651743)
-print(type(res))
 
-print(len(res["jobs"]))
+target_jobs = ["unit-test-job", "cypress-run-job"]
+
+output_lines = []
 
 for x in res["jobs"]:
-    if x["name"] == "unit-test-job" or  x["name"] == "cypress-run-job":
-        print(x["name"])
-        print(x["html_url"])
-        print(x["status"])
+    if x["name"] in target_jobs:
+        output_lines.append(f"Job Name: {x['name']}")
+        output_lines.append(f"HTML URL: {x['html_url']}")
+        output_lines.append(f"Status: {x['status']}")
+        output_lines.append("---")
+
+output = "\n".join(output_lines)  # Combine lines with newlines
+print(f"::set-output name=my_output::{output}")  # Set output in single line

02b468164925d6ea009a8f8f6c5c3233a36e09bf
iamkishorekumar-git <iamkishorekumar1@gmail.com>
2024-01-14 18:57:53 +0530

    Updated the res status in main.py file

--- main.py
+++ main.py
@@ -10,17 +10,12 @@ run_id = os.environ.get("RUN_ID")
 job_name_1 = os.environ.get("JOB_NAME_1")
 job_name_2 = os.environ.get("JOB_NAME_2")
 
-print("[job_name_1]",job_name_1)
-print("[job_name_2]",job_name_2)
-
 target_jobs = [job_name_1,job_name_2]
 print("[target_jobs]",target_jobs)
 print("[run_id]",run_id)
 
 
 res = workflow.getWorkflowJobs(org,repo,github_token,run_id)
-
-target_jobs = ["unit-test-job", "cypress-run-job"]
 
 output_jobs = []
 

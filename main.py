16a286a4db611fd080b6572c2d81153d2b70f58f
iamkishorekumar-git <iamkishorekumar1@gmail.com>
2024-01-14 07:04:24 +0530

    Updated inputs envs

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ repo = os.environ.get("REPO")
 github_token = os.environ.get("GH_TOKEN")
 run_id = os.environ.get("RUN_ID")
 job_name_1 = os.environ.get("JOB_NAME_1")
-job_name_2 = os.environ.get("JOB_NAME_2")
+job_name_1 = os.environ.get("JOB_NAME_2")
 
+print("[job_name_1]",job_name_1)
+print("[job_name_2]",job_name_2)
 
 target_jobs = [job_name_1,job_name_2]
 print("[target_jobs]",target_jobs)

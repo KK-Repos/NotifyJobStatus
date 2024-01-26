c89a0fb786334ea2de89ab96f38275771d6ac146
Kishore Kumar <55937175+KK-Repos@users.noreply.github.com>
2024-01-26 21:22:51 +0530

    Adding custom link

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ github_token = os.environ.get("GH_TOKEN")
 run_id = os.environ.get("RUN_ID")
 job_name_1 = os.environ.get("JOB_NAME_1")
 job_name_2 = os.environ.get("JOB_NAME_2")
+select_job = os.environ.get("TARGET_JOB")
+modify_job = os.environ.get("MODIFY_JOB")
+customLink = os.environ.get("CUSTOM_LINK")
 CHANNEL_ID = os.environ.get("CHANNEL_ID") 
-
+html_url = None 
 target_jobs = [job_name_1,job_name_2]
 
 
@@ -18,15 +21,20 @@ getJobResponse = workflow.getWorkflowJobs(org,repo,github_token,run_id)
 
 output_jobs = []
 
-for job in getJobResponse["jobs"]:
-    if job["name"] in target_jobs:
+if customLink and select_job:
+    print("inside")
+    for x in res["jobs"]:
+        if x["name"] in select_job:
+            html_url=x["html_url"]
+
+for x in res["jobs"]:
+    if x["name"] in target_jobs:
         job_info = {
-            "Job Name": job["name"],
-            "HTML URL": job["html_url"],
-            "Status": job["conclusion"]
+            "Job Name": x["name"],
+            "HTML URL": html_url if (customLink and x["name"] == modify_job and x["conclusion"] == "failure") else x["html_url"],
+            "Status": x["conclusion"]
         }
         output_jobs.append(job_info)
-
 
 output_file = os.getenv('GITHUB_OUTPUT')
     
